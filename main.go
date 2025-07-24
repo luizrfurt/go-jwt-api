c1d6b83648529d1afc0810f97253aa440f63412b
luizrfurt <luizrobertorfurt98@gmail.com>
2025-07-24 15:32:45 -0300

    use cors

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
+// main.go
 package main
 
 import (
 	"go-jwt-api/db"
 	"go-jwt-api/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: false,
+	}))
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "API is running"})
