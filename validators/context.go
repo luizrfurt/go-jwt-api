386e319c36e4a1f541c669068b05df16bad6ddf4
luizrfurt <luizrobertorfurt98@gmail.com>
2025-08-05 15:23:29 -0300

    remove updatecontext

--- validators/context.go
+++ validators/context.go
@@ -3,12 +3,7 @@ package validators
 
 type CreateContextRequest struct {
 	Name        string `json:"name" validate:"required,min=3,max=50"`
-	Description string `json:"description" validate:"max=200"`
-}
-
-type UpdateContextRequest struct {
-	Name        string `json:"name" validate:"required,min=3,max=50"`
-	Description string `json:"description" validate:"max=200"`
+	Description string `json:"description" validate:"required,max=200"`
 }
 
 type SelectContextRequest struct {
